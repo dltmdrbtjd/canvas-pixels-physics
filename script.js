d00f5d65e083810bbcde0a1fae99d2c421b2d4be
dltmdrbtjd <dltmdrbtjd@gmail.com>
2022-10-24 01:13:18 +0900

    feat: finished the particle,effect

--- script.js
+++ script.js
@@ -5,21 +5,50 @@ window.addEventListener("load", function () {
   canvas.height = window.innerHeight;
 
   class Particle {
-    constructor(effect) {
+    constructor(effect, x, y, color) {
       this.effect = effect;
       this.x = Math.random() * this.effect.width;
-      this.y = Math.random() * this.effect.height;
-      this.size = 10;
-      this.vx = Math.random() * 2 - 1;
-      this.vy = Math.random() * 2 - 1;
+      this.y = 0;
+      this.originX = Math.floor(x);
+      this.originY = Math.floor(y);
+      this.color = color;
+      this.size = this.effect.gap;
+      this.vx = 0;
+      this.vy = 0;
+      this.friction = 0.95;
+      this.ease = 0.05;
+      this.dx = 0;
+      this.dy = 0;
+      this.distance = 0;
+      this.force = 0;
+      this.angle = 0;
     }
 
     draw(context) {
+      context.fillStyle = this.color;
       context.fillRect(this.x, this.y, this.size, this.size);
     }
     update() {
-      this.x += this.vx;
-      this.y += this.vy;
+      this.dx = this.effect.mouse.x - this.x;
+      this.dy = this.effect.mouse.y - this.y;
+      this.distance = this.dx * this.dx + this.dy * this.dy;
+      this.force = this.effect.mouse.radius / this.distance;
+
+      if (this.distance < this.effect.mouse.radius) {
+        this.angle = Math.atan2(this.dy, this.dx);
+        this.vx += this.force * Math.cos(this.angle);
+        this.vy += this.force * Math.sin(this.angle);
+      }
+
+      this.x +=
+        (this.vx *= this.friction) + (this.originX - this.x) * this.ease;
+      this.y +=
+        (this.vy *= this.friction) + (this.originY - this.y) * this.ease;
+    }
+    warp() {
+      this.x = Math.random() * this.effect.width;
+      this.y = Math.random() * this.effect.height;
+      this.ease = 0.05;
     }
   }
 
@@ -33,22 +62,45 @@ window.addEventListener("load", function () {
       this.centerY = this.height * 0.5;
       this.x = this.centerX - this.image.width * 0.5;
       this.y = this.centerY - this.image.height * 0.5;
-      this.gap = 5;
+      this.gap = 2;
+      this.mouse = {
+        radius: 3000,
+        x: undefined,
+        y: undefined,
+      };
+      window.addEventListener("mousemove", (event) => {
+        this.mouse.x = event.x;
+        this.mouse.y = event.y;
+      });
     }
 
     init(context) {
       context.drawImage(this.image, this.x, this.y);
       const pixels = context.getImageData(0, 0, this.width, this.height).data;
       for (let y = 0; y < this.height; y += this.gap) {
-        for (let x = 0; x < this.width; x += this.gap) {}
+        for (let x = 0; x < this.width; x += this.gap) {
+          const index = (y * this.width + x) * 4;
+          const red = pixels[index];
+          const green = pixels[index + 1];
+          const blue = pixels[index + 2];
+          const alpha = pixels[index + 3];
+          const color = "rgb(" + red + "," + green + "," + blue + ")";
+
+          if (alpha > 0) {
+            this.particlesArray.push(new Particle(this, x, y, color));
+          }
+        }
       }
     }
     draw(context) {
       this.particlesArray.forEach((particle) => particle.draw(context));
-      context.drawImage(this.image, this.x, this.y);
+      //   context.drawImage(this.image, this.x, this.y);
     }
     update() {
       this.particlesArray.forEach((particle) => particle.update());
+    }
+    warp() {
+      this.particlesArray.forEach((particle) => particle.warp());
     }
   }
 
@@ -61,5 +113,12 @@ window.addEventListener("load", function () {
     effect.update();
     requestAnimationFrame(animate);
   }
-  //   animate();
+  animate();
+
+  // warp button
+  const warpButton = document.getElementById("warpButton");
+  warpButton.addEventListener("click", function () {
+    console.log("1");
+    effect.warp();
+  });
 });
